f0e6b684668907dacac760a45ebf49122861b6b1
Nigel Lee <lee.nigel@hotmail.com>
2021-10-09 09:06:43 +0800

    feat: Add lox driver

---
+++ lox/src/lox.rs
@@ -0,0 +1,50 @@
+use std::io::prelude::*;
+use std::{fs, io, path, process};
+
+pub struct Lox {
+    error: Option<String>,
+}
+
+impl Lox {
+    pub fn new() -> Self {
+        Lox { error: None }
+    }
+
+    pub fn run_file(&self, path: path::PathBuf) {
+        let source = fs::read_to_string(path).expect("Unable to read file");
+        self.run(source);
+
+        if self.error.is_some() {
+            process::exit(1)
+        }
+    }
+
+    pub fn run_prompt(&mut self) {
+        let mut input = String::new();
+        let stdin = io::stdin();
+        loop {
+            print!("lox> ");
+            io::stdout().flush().expect("[ICE] Unable to flush stdout");
+            stdin.lock().read_line(&mut input).unwrap();
+            self.run(input.clone());
+            input.clear();
+            self.error = None;
+        }
+    }
+
+    pub fn report(&mut self, line: usize, message: String) {
+        println!("[line {}] Error: {}", line, message);
+        self.error = Some(message);
+    }
+
+    fn run(&self, source: String) {
+        let scanner = Scanner::new(); // TODO
+        let tokens: Vec<Token> = Vec::new(); //TODO
+        match scanner.scan_tokens() {
+            Ok(ts) => tokens = ts,
+            Err(err) => self.report(err.line(), format!("{}", err)),
+        }
+
+        println!("{:?}", tokens);
+    }
+}
--- lox/src/main.rs
+++ lox/src/main.rs
@@ -1,3 +1,17 @@
+mod lox;
+mod token;
+
+use std::{env, path};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut lox = lox::Lox::new();
+
+    if args.len() == 1 {
+        lox.run_prompt();
+    } else if args.len() == 2 {
+        lox.run_file(path::PathBuf::from(&args[1]));
+    } else {
+        println!("usage: lox [filename.lox]")
+    }
 }

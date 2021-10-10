dfb3ddfd0663964686f568efea9b95897f2cee4d
Nigel Lee <lee.nigel@hotmail.com>
2021-10-10 20:52:45 +0800

    feat: Base parser

---
+++ parser/src/parser.rs
@@ -0,0 +1,12 @@
+use lexer::token::Token;
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        Parser { tokens, current: 0 }
+    }
+}

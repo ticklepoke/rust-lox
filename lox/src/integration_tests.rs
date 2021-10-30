ce8946c4f6777fb4b281e826730e8a136188257a
Nigel Lee <lee.nigel@hotmail.com>
2021-10-30 11:38:11 +0800

    fix: Wrong test type

--- lox/src/integration_tests.rs
+++ lox/src/integration_tests.rs
@@ -1,18 +1,20 @@
 use crate::lox::Lox;
 use frontend::environment::Environment;
 use interpreter::interpreter::Interpreter;
+use std::cell::RefCell;
 use std::env;
 use std::path;
+use std::rc::Rc;
 
 #[test]
 fn run_file() {
     let mut lox = Lox::new();
     let env = Environment::new(None);
-    let mut interpreter = Interpreter::new(env);
+    let interpreter = Rc::new(RefCell::new(Interpreter::new(env)));
 
     let curr_dir = env::current_dir().expect("path");
     let mut file_path = path::PathBuf::new();
     file_path.push(curr_dir);
     file_path.push("../__fixtures__/scope.lox");
-    lox.run_file(file_path, &mut interpreter);
+    lox.run_file(file_path, interpreter);
 }
